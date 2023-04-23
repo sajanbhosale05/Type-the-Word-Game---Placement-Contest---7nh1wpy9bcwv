27b6278e757b26e2c68c1797060a2582c18cbde6
Sajan Bhosale <83913731+sajanbhosale05@users.noreply.github.com>
2023-04-23 16:19:05 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -9,12 +9,37 @@ function App() {
   const [userInput, setUserInput] = useState('');
   const [result, setResult] = useState('');
   const [index, setIndex] = useState(0);
+  
+  useEffect(()=>{
+    setWord(WORD_LIST[index]);
+    const timer=setTimeout(()=>{setTFlashWord(false)},500);
+    return()=>clearTimeout(timer)
+  })
+  
+  const handleInputChange= (e)=>{setUserInput(e.target.value);}
+  const handleFormSubmit= (e)=>{if(userInput===word){setResult("You won!");}else{setResult("You lost!")}e.preventDefault();}
+  
+  
+  const handleRestartClick=(e)=>{
+    if(index>WORD_LIST.length){
+      setIndex(0);
+    }
+    e.preventDefault();
+    setIndex(index+1);
+    setTFlashWord(true);
+    setUserInput("");
+
+  }
+  
+  const
+  
 
 
   return (
     <div class="mini-game-container">
       <h2 class="mini-game-title">Mini Game</h2>
       <p class="mini-game-word">{word}</p>
+{! flashWord && !result && 
       <form class="mini-game-form" onSubmit={handleFormSubmit}>
         <input class="mini-game-input" type="text" value={userInput} onChange={handleInputChange} />
         <button class="mini-game-button" type="submit">Check Answer</button>
